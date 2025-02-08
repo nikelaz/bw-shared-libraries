61ffa281e0bd9ef4ede818e1a220bd5dcedac0a9
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2025-02-08 13:21:07 +0200

    fix: currency formatting function was throwing when an invalid currency/locale was input

--- src/formatting-utils.ts
+++ src/formatting-utils.ts
@@ -1,23 +1,26 @@
 export class CurrencyFormatter {
-  numberFormat: Intl.NumberFormat;
+  numberFormat: Intl.NumberFormat | null;
   currency: string;
 
   constructor(currency: string, locale?: string) {
     this.currency = currency;
-    this.numberFormat = Intl.NumberFormat(locale, {
-      style: 'currency',
-      currency: currency,
-    });
+
+    try {
+      this.numberFormat = Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency: currency,
+      });
+    }
+    catch {
+      this.numberFormat = null;
+    }
   }
 
   format(input: number) {
-    let output;
-    try {
-      output = this.numberFormat.format(input);
-    } catch (error) {
-      output = `${this.currency} ${input}`;
-    } finally {
-      return output;
+    if (this.numberFormat === null) {
+      return `${this.currency} ${input}`;
     }
+
+    return this.numberFormat.format(input);
   }
 }
